0caf70bd037b00bcdc3dab34d9f66bf666055207
Matias Vazquez-Pinon <matias.vazquez@tec.mx>
2021-03-31 00:26:18 -0600

    added MPLAB project for 7-seg display driver

---
+++ Lab05/LAB05.X/main.c
@@ -0,0 +1,80 @@
+////+++++++++++++++++++++++++++++++++++++| LIBRARIES / HEADERS |+++++++++++++++++++++++++++++++++++++
+#include "device_config.h"
+#include <stdint.h>
+#include <math.h>
+
+//+++++++++++++++++++++++++++++++++++++| DIRECTIVES |+++++++++++++++++++++++++++++++++++++
+#define _XTAL_FREQ 8000000
+#define SWEEP_FREQ 30
+
+//+++++++++++++++++++++++++++++++++++++| DATA TYPES |+++++++++++++++++++++++++++++++++++++
+enum por_dir{ output, input };              // output = 0, input = 1
+enum por_ACDC { digital, analog };          // digital = 0, analog = 1
+enum resistor_state { set_ON, res_ON };     // set_ON = 0, res_ON = 1
+enum led_state { led_OFF, led_ON };         // led_OFF = 0, led_ON = 1
+enum butto_state { pushed, no_pushed };     // pushed = 0, no_pushed = 1
+
+////+++++++++++++++++++++++++++++++++++++| ISRs |+++++++++++++++++++++++++++++++++++++
+//// ISR for high priority
+//void __interrupt ( high_priority ) high_isr( void );
+//// ISR for low priority
+//void __interrupt ( low_priority ) low_isr( void ); 
+
+//+++++++++++++++++++++++++++++++++++++| FUNCTION DECLARATIONS |+++++++++++++++++++++++++++++++++++++
+void portsInit(void);
+uint8_t char_to_seg(uint8_t);
+void send_to_disp(uint32_t);
+
+//+++++++++++++++++++++++++++++++++++++| MAIN |+++++++++++++++++++++++++++++++++++++
+void main(void){
+    portsInit();
+    while(1){
+        uint32_t num = 0x08070605;
+        send_to_disp(num);
+    }
+}
+
+//+++++++++++++++++++++++++++++++++++++| FUNCTIONS |+++++++++++++++++++++++++++++++++++++
+void portsInit(void){
+    ANSELA = digital;   // Set port A as Digital for keypad driving
+    TRISA  = 0x0F;      // For Port A, set pins 4 to 7 as outputs (rows), and pins 0 to 3 as inputs (cols)
+    ANSELB = digital;   // Set port B as Digital for 7 segment cathode selection (only 4 pins used)  
+    TRISB  = 0x00;      // For Port B, set pins as outputs for cathode selection
+    ANSELD = digital;   // Set port D as Digital for 7 segment anodes
+    TRISD  = 0x00;      // for Port D, set all pins as outputs for 7 segment anodes
+    OSCCON = 0b01110100;// Set the internal oscillator to 8MHz and stable
+}
+
+void send_to_disp(uint32_t disp_word){
+    for (char i = 0; i < 4; i++){
+        int internal_sweep = (int) pow(2, i);
+        int sweep = 0x0F & ~internal_sweep;
+        LATB = (char) sweep;
+        uint8_t num_disp = 0x000000FF & (disp_word >> i*8);
+        LATD = char_to_seg(num_disp);
+        __delay_ms(SWEEP_FREQ);
+    }
+}
+
+uint8_t char_to_seg(uint8_t num){
+    uint8_t segments;
+    switch(num){
+        case 0:  segments = 0b00111111; break;
+        case 1:  segments = 0b00000110; break;
+        case 2:  segments = 0b01011011; break;
+        case 3:  segments = 0b01001111; break;
+        case 4:  segments = 0b01100110; break;
+        case 5:  segments = 0b01101101; break;
+        case 6:  segments = 0b01111101; break;
+        case 7:  segments = 0b00000111; break;
+        case 8:  segments = 0b01111111; break;
+        case 9:  segments = 0b01100111; break;
+        case 10: segments = 0b01110111; break;
+        case 11: segments = 0b01111100; break;
+        case 12: segments = 0b01011000; break;
+        case 13: segments = 0b01011110; break;
+        case 14: segments = 0b01111001; break;
+        default: segments = 0b01110001; break;
+    }
+    return segments;
+}
